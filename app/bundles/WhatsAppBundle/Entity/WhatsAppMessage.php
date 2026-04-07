a49ef1f2fa45abd054473f5a7fd06b1fe283a1eb
iamjpsingh <190072362+iamjpsingh@users.noreply.github.com>
2026-04-07 11:20:21 +0000

    Fix: change property types from 'array' to '?array' to accept null from form mapper

--- app/bundles/WhatsAppBundle/Entity/WhatsAppMessage.php
+++ app/bundles/WhatsAppBundle/Entity/WhatsAppMessage.php
@@ -56,7 +56,7 @@ class WhatsAppMessage extends FormEntity implements UuidInterface
     /**
      * @var array<string, mixed>
      */
-    private array $templateComponents = [];
+    private ?array $templateComponents = [];
 
     private ?string $mediaUrl = null;
 
@@ -67,7 +67,7 @@ class WhatsAppMessage extends FormEntity implements UuidInterface
     /**
      * @var array<string, mixed>
      */
-    private array $interactiveData = [];
+    private ?array $interactiveData = [];
 
     private int $sentCount = 0;
 
