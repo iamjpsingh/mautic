1f2e8738d78768b38dfc7a7a3a111b57aa44d6d2
iamjpsingh <190072362+iamjpsingh@users.noreply.github.com>
2026-04-09 11:05:48 +0000

    Fix WhatsAppListType: use EntityLookupType to load messages in campaign/form dropdowns, add missing translations

--- app/bundles/WhatsAppBundle/Form/Type/WhatsAppListType.php
+++ app/bundles/WhatsAppBundle/Form/Type/WhatsAppListType.php
@@ -4,18 +4,49 @@ declare(strict_types=1);
 
 namespace Mautic\WhatsAppBundle\Form\Type;
 
+use Mautic\CoreBundle\Form\Type\EntityLookupType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
- * Provides a select list of WhatsApp message entities.
- *
  * @extends AbstractType<array<mixed>>
  */
 class WhatsAppListType extends AbstractType
 {
-    public function getParent(): string
+    public function configureOptions(OptionsResolver $resolver): void
     {
-        return ChoiceType::class;
+        $resolver->setDefaults(
+            [
+                'modal_route'         => 'mautic_whatsapp_action',
+                'modal_header'        => 'mautic.whatsapp.header.new',
+                'model'               => 'whatsapp',
+                'model_lookup_method' => 'getLookupResults',
+                'lookup_arguments'    => fn (Options $options): array => [
+                    'type'    => WhatsAppType::class,
+                    'filter'  => '$data',
+                    'limit'   => 0,
+                    'start'   => 0,
+                    'options' => [
+                        'ignore_ids' => $options['ignore_ids'],
+                    ],
+                ],
+                'ajax_lookup_action' => function (Options $options): string {
+                    $query = [
+                        'ignore_ids' => $options['ignore_ids'],
+                    ];
+
+                    return 'whatsapp:getLookupChoiceList&'.http_build_query($query);
+                },
+                'multiple'   => false,
+                'required'   => false,
+                'ignore_ids' => [],
+            ]
+        );
+    }
+
+    public function getParent(): ?string
+    {
+        return EntityLookupType::class;
     }
 }
